d1556cfc3d825403567710036190b74c2df556dd
eliasnsz <eliasnsz.dev@gmail.com>
2024-04-17 16:29:59 -0300

    :wrench: chore: font and styles configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,30 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			backgroundImage: {
+				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+				"gradient-conic":
+					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+			},
+			colors: {
+				background: "#F3F3F3",
+			},
+			boxShadow: {
+				primary: "0px 4px 0px black",
+			},
+			fontFamily: {
+				title: "var(--font-title)",
+				body: "var(--font-body)",
+			},
+		},
+	},
+	plugins: [],
 };
 export default config;
